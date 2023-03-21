8ffddb79fb0826fa6e50036d790fa4cbc39fe03b
Nethmini__0_11 <manthikaoriginal@gmail.com>
2023-03-21 14:40:05 +0530

    Create Login.style.js

---
+++ FrontEnd/src/features/user/Login.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LoginImageContainer = styled.View`
+    width: 100%;
+    height: auto;
+    margin: 10px 0;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+`;
