2b2d8eaef37fd11d6ee0015e855f1df51affd2e4
Nethmini__0_11 <manthikaoriginal@gmail.com>
2023-03-23 13:34:28 +0530

    implement update and delete methods

--- BackEnd/Controller/trip.js
+++ BackEnd/Controller/trip.js
@@ -51,3 +51,41 @@ exports.getTrip = async (req, res) => {
     }
 }
 
+//get all trip by object uid
+exports.getTripDetailsByUid = async (req, res) => {
+    try {
+        uid = req.body.uid;
+        if (!uid) {
+            return res.status(400).json({ message: "Please enter all fields" });
+        }
+        const trip = await Trip.find(
+            { uid: uid }
+        );
+        res.send({ message: "success", trip: trip });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+//update Trip
+exports.updateTrip = async (req, res) => {
+    try {
+        const trip = await Trip.findByIdAndUpdate(req.params.id, req.body);
+        res.send({ message: "success", trip });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+//delete Trip
+exports.deleteTrip = async (req, res) => {
+    try {
+        const trip = await Trip.findByIdAndDelete(req.params.id);
+        res.send({ message: "success", trip });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
