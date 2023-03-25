d6a4d25da9d272e7b35d6f07d726a6a6815d65c0
123kavi <kavihansi98@gmail.com>
2023-03-25 12:16:44 +0530

    EVENTS

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -8,7 +8,12 @@ const URL = process.env.MONGODB_URL;
 const cors = require("cors");
 
 const userRouter = require("./Routers/user");
+
 const EventRouter = require("./Routers/event");
+
+const tripRouter = require("./Routers/trip");
+const transportationRouter = require("./Routers/transportation");
+const AccommodationRouter = require('./Routers/accommodationRoute');
 
 const tripRouter = require("./Routers/trip");
 const transportationRouter = require("./Routers/transportation");
@@ -24,11 +29,17 @@ mongoose.connect(URL, (err) => {
 });
 
 app.use("/api/user", userRouter); //user login & Registration
+
 app.use("/api/event", EventRouter);
 
 app.use("/api/trip", tripRouter); //trip
 app.use("/api/transportation", transportationRouter);
 app.use("/api/accommodations", AccommodationRouter); 
+
+app.use("/api/trip", tripRouter); //trip
+app.use("/api/transportation", transportationRouter);
+app.use("/api/accommodations", AccommodationRouter); //Accommodations
+
 //! create server with port number
 app.listen(process.env.PORT || "0.0.0.0", () => {
   console.log(`service is up and running on port ${PORT}`);
