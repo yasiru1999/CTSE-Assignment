5b8b4cb3a0a166b6c318633710eb9aade4ccdcc2
Oshen Gomes <87510830+OshenGomes@users.noreply.github.com>
2023-03-25 22:51:49 +0530

    minor changes

--- FrontEnd/src/features/transportation/pages/AddTransportation.js
+++ FrontEnd/src/features/transportation/pages/AddTransportation.js
@@ -11,6 +11,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function AddTransportation({ navigation }) {
 
+
   const [userId, setUserId] = useState('');
   const [title, setTitle] = useState('');
   const [origin, setOrigin] = useState('');
@@ -85,34 +86,42 @@ function AddTransportation({ navigation }) {
         <IconView></IconView>
       </TopNav>
       <ScrollView style={{ marginBottom: "20%" }}>
+        
         <InputView>
           <Title>Title</Title>
           <Input placeholder={"Enter Title"} onChangeText={value => setTitle(value)} />
         </InputView>
+
         <InputView>
           <Title>Origin</Title>
           <Input placeholder={"Enter Origin"} onChangeText={value => setOrigin(value)} />
         </InputView>
+
         <InputView>
           <Title>Destination</Title>
           <Input placeholder={"Enter Destination"} onChangeText={value => setDestination(value)} />
         </InputView>
+
         <InputView>
           <Title>Contact number</Title>
           <Input placeholder={"Enter contact number"} onChangeText={value => setTravelDates(value)}  />
         </InputView>
+
         <InputView>
           <Title>Type Of Transportation</Title>
           <Input placeholder={"Enter Type Of Transportation"} onChangeText={value => setTypeOfTransportation(value)}  />
         </InputView>
+
         <InputView>
           <Title>Travel Class</Title>
           <Input placeholder={"Enter Description"} onChangeText={value => setTravelClass(value)}  />
         </InputView>
+
         <InputView>
           <Title>Date</Title>
           <Input placeholder={"Enter Date"} onChangeText={value => setDate(value)} />
         </InputView>
+
         <InputView>
           <Title>Image</Title>
           <Input placeholder={"Enter Image Link"} onChangeText={value => setImage(value)} />
@@ -122,9 +131,11 @@ function AddTransportation({ navigation }) {
           <Icon name="exclamation-circle" size={16} color="#42a1f5" />
           By Clicking Create You Agree to take full resposibility for this details and are under the privacy & policy of this and government law.
         </Caution>
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Button title="Create" color="#42a1f5" onPress={() => { handleSubmitData() }} />
         </View>
+
       </ScrollView>
       <NavigationBottomBar navigation={navigation} />
     </Container>
