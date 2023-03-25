7fc6c3ed824ef103511d83a9b6d5663c3a49fa98
Nethmini__0_11 <manthikaoriginal@gmail.com>
2023-03-25 09:47:32 +0530

    Update App.js

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -3,11 +3,6 @@ import Register from "./src/features/user/Register";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./src/features/home/Home";
-import AddEvent from "./src/features/event/pages/AddEvent";
-import AllEvent from "./src/features/event/pages/AllEvent";
-import MyEvent from "./src/features/event/pages/MyEvent";
-import EditEvent from "./src/features/event/pages/EditEvent";
-
 import TripAdd from "./src/features/trip/TripAdd";
 import MyTrips from "./src/features/trip/MyTrips";
 import TripHome from "./src/features/trip/TripHome";
@@ -15,7 +10,6 @@ import TripEdit from "./src/features/trip/TripEdit";
 import TripCreate from "./src/features/trip/TripCreate";
 import TripUserView from "./src/features/trip/TripUserView";
 import HomeWelcome from "./src/features/home/HomeWelcome";
-
 
 
 
@@ -32,14 +26,10 @@ export default function App() {
             headerShown: false
           }
         }>
-        <Stack.Screen name="Login" component={Login} />
+       <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="addevent" component={AddEvent} />
-         <Stack.Screen name="allevent" component={AllEvent} />
-        <Stack.Screen name="myevent" component={MyEvent} />
-         <Stack.Screen name="editevent" component={EditEvent} />
-         <Stack.Screen name="HomeWelcome" component={HomeWelcome} />
+        <Stack.Screen name="HomeWelcome" component={HomeWelcome} />
         <Stack.Screen name="TripHome" component={TripHome} />
         <Stack.Screen name="MyTrips" component={MyTrips} />
         <Stack.Screen name="TripAdd" component={TripAdd} />
